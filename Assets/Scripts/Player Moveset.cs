bc1243c7f4a32ba9828adc89150d93c5e73ff21f
leo <bzzz.crocs@gmail.com>
2026-07-24 15:59:32 +0200

    Initial commit

---
+++ Assets/Scripts/Player Moveset.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    public void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(moveHorizontal, moveVertical) * speed;
+        spriteRenderer.flipX = rb.linearVelocity.x < 0;
+
+
+        if (moveHorizontal != 0){
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+
+        if (moveVertical != 0 & rb.linearVelocity.y < 0)
+        {
+            animator.SetBool("isDown", true);
+        }
+        else
+        {
+            animator.SetBool("isDown", false);
+        }
+
+
+        if (moveVertical != 0 & rb.linearVelocity.y > 0)
+        {
+            animator.SetBool("isUp", true);
+        }
+        else
+        {
+            animator.SetBool("isUp", false);
+        }
+    }
+}
