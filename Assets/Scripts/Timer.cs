33e84b8bfb9eeeaabbec6ad3699578a81037126e
leo <bzzz.crocs@gmail.com>
2026-07-25 18:58:40 +0200

    timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI countdownText;
+    public float remainingTime;
+
+    private void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        countdownText.text = string.Format("{0:00}:{1:00}",minutes, seconds);
+
+        
+    }
+
+}
