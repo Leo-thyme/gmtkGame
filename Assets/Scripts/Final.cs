42c91ae209e7160aa19bddb23ec4938a01dc1d79
leo <bzzz.crocs@gmail.com>
2026-07-26 13:28:21 +0200

    dd

--- Assets/Scripts/Final.cs
+++ Assets/Scripts/Final.cs
@@ -3,29 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class Final : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
+  
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             SceneManager.LoadScene(1);
+          
         }
     }
 
-    public void FinishedLevel()
-    {
-        SceneManager.LoadScene(1);
-    }
 
-    
-    
 }
