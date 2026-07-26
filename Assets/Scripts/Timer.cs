cc19e135fc9e9284e418daa7338c1c7771379536
leo <bzzz.crocs@gmail.com>
2026-07-26 15:29:18 +0200

    ggg

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -7,21 +8,56 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI countdownText;
     public float remainingTime;
     public Animator animator;
+    
+    public GameObject player;
+    
+
+    NewMonoBehaviourScript dzemikBoosterActive;
+    bool boosterActive = false;
+
+    public GameObject UIbooster1;
+    BoosterScript scriptBooster1;
+    float boosterTimer = 0f;
+
+    public void Awake()
+    {
+        dzemikBoosterActive = player.GetComponent<NewMonoBehaviourScript>();
+
+        scriptBooster1 = UIbooster1.GetComponent<BoosterScript>(); 
+
+    }
 
     private void Update()
     {
-        if (remainingTime > 0)
+        if (dzemikBoosterActive.BoosterActive == false)
         {
-            remainingTime -= Time.deltaTime;
-        }
-        else if (remainingTime < 0)
-        {
-            remainingTime = 0;
-            
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            else if (remainingTime < 0)
+            {
+                remainingTime = 0;
 
+                
+            }
         }
-        
-     
+        else
+        {
+            if(scriptBooster1.boosterTimer >= 1)
+            {
+                boosterActive = true;
+            }
+            else
+            {
+                boosterActive = false;
+
+            }
+        }
+
+
+
+
 
             int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
