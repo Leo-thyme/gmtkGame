46a9eea9a3122537fe6c88f0033d3ccf1702cfe6
leo <bzzz.crocs@gmail.com>
2026-07-26 14:01:11 +0200

    nnnn

--- Assets/Scripts/Final.cs
+++ Assets/Scripts/Final.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class Final : MonoBehaviour
 {
-  
+    int nextSceneIndex = 0;
+
+    public void Awake()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(1);
-          
+            SceneManager.LoadScene(nextSceneIndex);
+
         }
     }
 
